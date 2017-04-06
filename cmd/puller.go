6894a667061b345d8757ad65a375e4281711632e
yuvipanda <yuvipanda@gmail.com>
2017-04-06 11:18:28 -0700

    Support authentication in puller + exit properly if pulling fails
    - Support passing in auth credentials via commandline flags.
    - If we get an error in the last status we recieve, we exit with
      a non-zero status code.

--- cmd/puller.go
+++ cmd/puller.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -12,7 +14,37 @@ import (
 )
 
 func main() {
+	username := flag.String("username", "", "Username to pass to docker for authentication with registry")
+	password := flag.String("password", "", "Password to pass to docker for authentication with registry")
+	email := flag.String("email", "", "Email to pass to docker for authentication with registry")
+
 	flag.Parse()
+
+	imagePullOptions := types.ImagePullOptions{}
+	if *username != "" {
+		if *password == "" {
+			fmt.Println("Password can not be empty if username is specified")
+			os.Exit(1)
+		}
+		if *email == "" {
+			fmt.Println("Email can not be empty if username is specified")
+			os.Exit(1)
+		}
+
+		auth := map[string]string{
+			"username": *username,
+			"password": *password,
+			"email":    *email,
+		}
+
+		authBytes, err := json.Marshal(auth)
+		if err != nil {
+			// This can actually never happen, unless there's a bug in the json package
+			panic(err)
+		}
+
+		imagePullOptions.RegistryAuth = base64.StdEncoding.EncodeToString(authBytes)
+	}
 
 	imageSpec := flag.Arg(0)
 	if imageSpec == "" {
@@ -21,6 +53,7 @@ func main() {
 	}
 	ctx := context.Background()
 
+	// The image pull API is probably not going to change for quite a while!
 	// This is a hack, but easier than constructing a client manually
 	os.Setenv("DOCKER_API_VERSION", "1.24")
 
@@ -29,14 +62,29 @@ func main() {
 		panic(err)
 	}
 
-	pullInfo, err := cli.ImagePull(ctx, imageSpec, types.ImagePullOptions{})
+	pullInfo, err := cli.ImagePull(ctx, imageSpec, imagePullOptions)
 	if err != nil {
+		// If it doesn't succeed, just error out
 		panic(err)
 	}
 	defer pullInfo.Close()
 
 	pullInfoScanner := bufio.NewScanner(pullInfo)
+
+	lastLine := ""
 	for pullInfoScanner.Scan() {
-		fmt.Println(pullInfoScanner.Text())
+		lastLine = pullInfoScanner.Text()
+		fmt.Println(lastLine)
+	}
+
+	var lastMessageParsed map[string]interface{}
+
+	err = json.Unmarshal([]byte(lastLine), &lastMessageParsed)
+	if err != nil {
+		panic(err)
+	}
+
+	if _, errorPresent := lastMessageParsed["error"]; errorPresent {
+		os.Exit(1)
 	}
 }
