c0e462025de97bccb66f69cc30cf854a9e2e5bf2
yuvipanda <yuvipanda@gmail.com>
2017-04-06 18:40:37 -0700

    Make allnodes use the shell scripted puller
    It looks like the auth tokens you can get from the gcr.io metadata
    service are per-node, so this auth token retreiving logic needs
    to live in the puller. Since it doesn't live in the go puller
    right now, let's just use the bash puller until that gets fixed

--- pull-allnodes/allnodes.sh
+++ pull-allnodes/allnodes.sh
@@ -3,21 +3,24 @@ set -e
 
 IMAGE="${1}"
 TAG="${2}"
-IMAGESPEC="${IMAGE}:${TAG}"
 NODECOUNT=$(kubectl get node | awk '{ if ($2 == "Ready") print $1; }'  | wc -l)
 
-JOBNAME=$(echo -n "pull-${IMAGE}-${TAG}-$(date +'%s')" | sed 's/\./-/g')
+IMAGESHORTNAME=$(echo -n ${IMAGE} | cut -d'/' -f3)
+JOBNAME=$(echo -n "pull-${IMAGESHORTNAME}-${TAG}-$(date +'%s')" | sed 's/\./-/g')
+
+echo "Pulling ${IMAGE}:${TAG} on ${NODECOUNT} nodes" 
 
 cat pulljob.yaml \
     | sed "s/{NODECOUNT}/${NODECOUNT}/" \
     | sed "s/{JOBNAME}/${JOBNAME}/" \
-    | sed "s/{IMAGESPEC}/${IMAGESPEC}/" \
+    | sed "s_{IMAGE}_${IMAGE}_" \
+    | sed "s_{TAG}_${TAG}_" \
     | kubectl apply -f -
 
 while true; do
     sleep 2 # Hack because .succeeded isn't populated instantly
     SUCCESSCOUNT=$(kubectl get job ${JOBNAME} -o jsonpath='{.status.succeeded}')
-    if [ ${SUCCESSCOUNT} = ${NODECOUNT} ]; then
+    if [ ${SUCCESSCOUNT} -eq ${NODECOUNT} ]; then
         echo "All nodes pulled successfully!"
         exit 0
     fi
